22b1872f8be60f1b31bd13a226de249ad9c93f9a
Ning <jianing0702@gmail.com>
2022-10-26 16:41:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write experimental main programs here
+
         PaymentTerminal unicafeExactum = new PaymentTerminal();
         System.out.println(unicafeExactum);
 
